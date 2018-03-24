f2f6946ceccfd3968fcf74176d6862aca93b6218
Ryan Hu <ryanhuuu@gmail.com>
2018-03-24 20:35:32 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,58 @@
-# your code goes here
+def begins_with_r(tools)
+  boo = true
+  tools.each { |tool|
+    if tool[0] != "r" && tool[0] != "R"
+      boo = false
+    end
+  }
+  boo
+end
+
+def contain_a(arr)
+
+  return arr.select { |ele|
+    ele.include?("a")
+  }
+end
+
+def first_wa(arr)
+  return arr.find { |ele|
+    ele[0]=="w" && ele[1]=="a"
+  }
+end
+
+def remove_non_strings(arr)
+  arr.select { |ele|
+    ele.is_a?(String)
+  }
+end
+
+def count_elements(justWhy)
+  stepResult = {}
+  result = []
+  justWhy.each do |ele|
+    if !stepResult.has_key?(ele[:name])
+      stepResult[ele[:name]] = 1
+    else
+      stepResult[ele[:name]] += 1
+    end
+  end
+  
+  stepResult.each do |name, count|
+    result << {name: name, count: count }
+  end
+  
+  
+  
+  return result
+end
+
+def merge_data(a,b)
+  result = []
+  b[0].each do |key, val|
+    
+  end
+  
+  
+  return a
+end
